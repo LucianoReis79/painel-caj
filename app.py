40c26a8f07dc70595fa1c8d3f6056b3d7e6067d4
LucianoReis79 <lucianoreis79@gmail.com>
2026-02-22 16:16:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -262,7 +262,7 @@ if pagina == "Lista de Pacientes":
 
     st.dataframe(df_filtrado, use_container_width=True)
 
-    csv = df_filtrado.to_csv(index=False, sep=";", encoding="utf-8")
+    csv = df_filtrado.to_csv(index=False, sep=";", encoding="utf-8-sig")
     st.download_button(
         "Baixar Lista",
         csv,
@@ -313,7 +313,7 @@ elif pagina == "Resumo por Medicamento":
 
     st.dataframe(resumo, use_container_width=True)
 
-    csv = resumo.to_csv(index=False, sep=";", encoding="utf-8")
+    csv = resumo.to_csv(index=False, sep=";", encoding="utf-8-sig")
     st.download_button(
         "Baixar Resumo",
         csv,
